2235a056455c15be493655bdd2a043251aac51fe
AAmanzi <alnick.amanzi@gmail.com>
2019-04-27 23:32:30 +0200

    Finished styling the main page

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,22 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./MainScreen.css";
 
-const MainScreen = () => {
-  return (
-    <div className="MainScreenWrapper">
-      <div className="MainScreenCats">
-        <Link to="/cats">
-          <button>CATS</button>
-        </Link>
+class MainScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePanel: null
+    };
+  }
+
+  showLeftPanel = () => {
+    this.setState({ activePanel: "left" });
+  };
+
+  showRightPanel = () => {
+    this.setState({ activePanel: "right" });
+  };
+
+  resetPanels = () => {
+    this.setState({ activePanel: null });
+  };
+
+  render() {
+    return (
+      <div className="MainScreenWrapper">
+        <span
+          className={`PanelLeft ${
+            this.state.activePanel === "left" ? "PanelLeftActive" : ""
+          }`}
+        />
+        <span
+          className={`PanelRight ${
+            this.state.activePanel === "right" ? "PanelRightActive" : ""
+          }`}
+        />
+        <div className="MainScreenCats">
+          <Link to="/cats">
+            <button
+              onMouseOver={this.showLeftPanel}
+              onMouseOut={this.resetPanels}
+            >
+              CATS
+            </button>
+          </Link>
+        </div>
+        <div className="MainScreenDogs">
+          <Link to="/dogs">
+            <button
+              onMouseOver={this.showRightPanel}
+              onMouseOut={this.resetPanels}
+            >
+              DOGS
+            </button>
+          </Link>
+        </div>
       </div>
-      <div className="MainScreenDogs">
-        <Link to="/dogs">
-          <button>DOGS</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MainScreen;
